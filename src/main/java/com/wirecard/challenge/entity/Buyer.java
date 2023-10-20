2b8926df906e3351891e490fd03a0edf459d921d
tiago <tgribeiro@gmail.com>
2023-10-20 16:46:43 -0300

    feat: creating entity Buyer

---
+++ src/main/java/com/wirecard/challenge/entity/Buyer.java
@@ -0,0 +1,33 @@
+package com.wirecard.challenge.entity;
+
+import com.wirecard.challenge.dto.CreatePaymentDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity(name = "buyers")
+@Table(name = "buyers")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Buyer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Email
+    private String email;
+    @Column(unique = true)
+    private String cpf;
+
+    public Buyer(CreatePaymentDTO dto) {
+        this.name = dto.name();
+        this.email = dto.email();
+        this.cpf = dto.cpf();
+    }
+
+}
